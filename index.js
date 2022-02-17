4ad537e1c61502b5e74929833b3dec0512bb76fd
Seongjae <robbyl95@hanmail.net>
2022-02-17 10:57:18 +0900

    몽고DB 연결

--- index.js
+++ index.js
@@ -4,7 +4,10 @@ const app = express();
 const port = 5000;
 
 const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://seongjae:1234@sjdb.eipk6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority');
+mongoose.connect('mongodb+srv://seongjae:1234@sjdb.eipk6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
+  useNewUrlParser: true, useUnifiedTopology: true
+}).then(() => console.log('MongoDB is Connected successfully~'))
+  .catch(err => console.log(err));
 
 app.get('/', (req, res) => {
   res.send('Hello U&E~~~~~~~!!!!!!!!!!!');
