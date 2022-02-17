258b25526847f835a5b3619a2359e7216cfc54ea
Seongjae <robbyl95@hanmail.net>
2022-02-17 17:21:02 +0900

    로그아웃 구현 완료

--- index.js
+++ index.js
@@ -81,10 +81,20 @@ app.get('/api/users/auth', auth, (req, res) => {
     role: req.user.role,
     image: req.user.image,
   });
-
 });
 
-
+app.get('/api/users/logout', auth, (req, res) => {
+  User.findOneAndUpdate(
+    { _id: req.user._id },
+    { token: "" },
+    (err, user) => {
+      if (err) return res.json({ success: false, err });
+      return res.status(200).send({
+        success: true
+      });
+    }
+  );
+});
 
 
 app.listen(port, () => {
